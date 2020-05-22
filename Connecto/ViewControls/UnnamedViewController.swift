27e0d38285d296639e1bb8c15e78294ecae1e7cd
Jerry Ren <yren@gm.slc.edu>
2020-05-22 16:22:27 -0400

    foundational

---
+++ Connecto/ViewControls/UnnamedViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ......    ViewController.swift
+//  Connecto
+//
+//  Created by Jerry Ren on 5/20/20.
+//  Copyright © 2020 Jerry Ren. All rights reserved.
+//
+
+import UIKit
+
+class KaoViewController: UIViewController {
+    @IBOutlet weak var centeredLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        centeredLabel.text = ""
+        let fullDisplay = "Flashy⚡️Connecto"
+        var unitCount = 0
+        
+        for textUnit in fullDisplay {
+            Timer.scheduledTimer(withTimeInterval: Double(unitCount) * 0.1, repeats: false) { (timer) in
+                self.centeredLabel.text?.append(textUnit)
+            }
+            unitCount += 1
+        }
+        
+    }
+
+
+}
+
---
+++ Connecto/Views/CurvedButtonExtension.swift
@@ -0,0 +1,32 @@
+//
+//  CurvedButtonExtension.swift
+//  Connecto
+//
+//  Created by Jerry Ren on 5/22/20.
+//  Copyright © 2020 Jerry Ren. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+
+@IBDesignable class CurvedButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = .clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
